e0c33c93942853683594bc28fe38828dc2d74b5b
First Last <kcruz101@fiu.edu>
2025-11-15 10:31:29 -0500

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function addItem() {
+    const item = prompt("New item:");
+    if (item){
+        const li = document.createElement("li");
+        li.textContent = item;
+        document.getElementById("list").appendChild(il);
+    }
+}
+
+function removeItem() {
+        const li = document.createElement("li");
+        const last = FileList.lastElementChild;
+        if (last) FileList.removeChild
+}
